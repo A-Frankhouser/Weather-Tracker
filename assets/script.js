e5fad92a503ea06176d624fe33ca3dac586d7888
Alex Frankhouser <freesoul423@gmail.com>
2022-10-24 21:32:20 -0400

    added the basic fetch response

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
-    // key for Open Weather
-APIKey = "417480d7c05aefc18c93459468066af6"
+// key for Open Weather
+// APIKey = "417480d7c05aefc18c93459468066af6"
+// ------------------------------------------------
+
+let weather = {
+    "apiKey": "417480d7c05aefc18c93459468066af6",
+    fetchWeather: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=woodstock&appid=417480d7c05aefc18c93459468066af6&units=imperial"
+        ).then((response) => response.json())
+        .then((data) => console.log(data))
+    }
+}
