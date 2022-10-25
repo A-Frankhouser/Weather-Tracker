f3a57438ea3e49476b2c33c825de79b6a43b8e19
Alex Frankhouser <freesoul423@gmail.com>
2022-10-25 18:07:49 -0400

    added to the JavaScript. Made it display the current weather when consologed

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,25 @@
 
 let weather = {
     "apiKey": "417480d7c05aefc18c93459468066af6",
-    fetchWeather: function() {
+    fetchWeather: function(city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=woodstock&appid=417480d7c05aefc18c93459468066af6&units=imperial"
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey + "&units=imperial"
         ).then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed} = data.wind;
+        const { timezone } = data;
+        console.log(name, icon, description, temp, humidity, speed, timezone);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        // document.querySelector(".currentDate").innerText = timezone;
+        document.querySelector(".currentIcon").src = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".current-temp").innerText = temp + " ℉";
+        document.querySelector(".current-wind").innerText = speed + " mph";
+        document.querySelector(".current-humidity").innerText = humidity + "%";
     }
 }
